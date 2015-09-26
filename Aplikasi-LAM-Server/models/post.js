662cffa6a0463d18fa1a4fc2df06f86c1057e491
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-26 18:29:01 +0700

    add domain post

---
+++ Aplikasi-LAM-Server/models/post.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var post;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+post = new Schema({
+  email: {
+    type: 'String',
+    required: true
+  },
+  nama: {
+    type: 'String',
+    required: true
+  },
+  isiPost: {
+    type: 'String',
+    required: true
+  },
+  tanggal: {
+    type: 'Date',
+    required: true
+  },
+  komentar: {
+    type: 'array',
+    required: false
+  },
+  like: {
+    type: 'array',
+    required: true
+  }
+}, {
+  collection: 'tb_post'
+});
+
+module.exports = mongoose.model('Post', post);
